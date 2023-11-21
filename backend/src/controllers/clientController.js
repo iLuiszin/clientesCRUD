c01d0874d813daca8e16a1b1d01ccda4b94ccd18
Luís Felipe <oluis10@gmail.com>
2023-11-21 00:45:12 -0300

    finishing CRUD

--- backend/src/controllers/clientController.js
+++ backend/src/controllers/clientController.js
@@ -6,6 +6,10 @@ const clientes = [
   { id: 5, nome: 'Luís' },
 ]
 
+const findClientById = (id) => {
+  return clientes.find((cliente) => cliente.id === parseInt(id))
+}
+
 const clientController = {
   getClients: (req, res) => {
     return res.status(200).send(clientes)
@@ -14,20 +18,24 @@ const clientController = {
   getOneClient: (req, res) => {
     const id = req.params.id
 
-    const cliente = clientes.find((cliente) => cliente.id == Number(id))
+    const cliente = findClientById(id)
 
-    if (cliente) {
-      return res.status(200).send(cliente)
+    if (!cliente) {
+      return res
+        .status(404)
+        .send({ message: 'Não foi encontrado um cliente para esse id' })
     }
 
-    return res.status(404).send('Cliente não encontrado')
+    return res.status(200).send(cliente)
   },
 
   createClient: (req, res) => {
     const { nome } = req.body
 
     if (!nome) {
-      return res.status(400).send('Nome não informado')
+      return res.status(400).send({
+        message: 'O campo nome é obrigatório',
+      })
     }
 
     const id = clientes[clientes.length - 1].id + 1
@@ -40,6 +48,45 @@ const clientController = {
     clientes.push(cliente)
 
     return res.status(201).send(cliente)
+  },
+
+  updateClient: (req, res) => {
+    const { id } = req.params
+    const { nome } = req.body
+
+    if (!nome) {
+      return res.status(400).send({
+        message: 'O campo nome é obrigatório',
+      })
+    }
+
+    const cliente = findClientById(id)
+
+    if (!cliente) {
+      return res
+        .status(404)
+        .send({ message: 'Não foi encontrado um cliente para esse id' })
+    }
+
+    cliente.nome = nome
+
+    return res.status(200).send(cliente)
+  },
+
+  removeClient: (req, res) => {
+    const { id } = req.params
+
+    const cliente = findClientById(id)
+
+    if (!cliente) {
+      return res
+        .status(404)
+        .send({ message: 'Não foi encontrado um cliente para esse id' })
+    }
+
+    clientes.splice(clientes.indexOf(cliente), 1)
+
+    return res.status(200).send({ message: 'cliente removido' })
   },
 }
 
